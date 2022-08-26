88d8abada5e72ea358cfab82e55a4fbf45c990ea
byeolyee <byeolyee2230@gmail.com>
2022-08-26 15:17:58 +0900

    second commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+//create by object literal
+const person1 = {
+    name : 'byeolyee',
+    age : 31
+}
+
+//create by object.create
+const person2 = Object.create({
+    name : 'byeolyee',
+    age : 31
+})
+
+console.log(typeof person1);
+console.log(typeof person2);
